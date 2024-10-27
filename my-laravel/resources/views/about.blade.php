cced5489824befd51fc361bfbd0291341bb0aa7a
aristos <aristos.aresti@gmail.com>
2024-10-27 22:37:40 +0200

    some small chasges

--- my-laravel/resources/views/about.blade.php
+++ my-laravel/resources/views/about.blade.php
@@ -2,5 +2,6 @@
     <x-slot:heading>
         About Page
     </x-slot:heading>
+
     <h1>About</h1>
 </x-layout>
--- my-laravel/resources/views/components/layout.blade.php
+++ my-laravel/resources/views/components/layout.blade.php
@@ -2,21 +2,12 @@
 <html class="h-full bg-gray-100">
 
 <head>
-    <title>Home</title>
+    <title>My Website</title>
     @vite('resources/css/app.css')
 
 </head>
 
 <body class="h-full">
-
-    <!--
-  This example requires updating your template:
-
-  ```
-  <html class="h-full bg-gray-100">
-  <body class="h-full">
-  ```
--->
     <div class="min-h-full">
         <nav class="bg-gray-800">
             <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -30,12 +21,10 @@
                         <div class="hidden md:block">
                             <div class="ml-10 flex items-baseline space-x-4">
                                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                                <a href="/" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"
-                                    aria-current="page">Home</a>
-                                <a href="/about"
-                                    class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
-                                <a href="/contact"
-                                    class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
+                                <x-nav-link-mine href="/" :active="request()->is('/')">Home</x-nav-link-mine>
+                                <x-nav-link-mine href="/about" :active="request()->is('about')">About</x-nav-link-mine>
+                                <x-nav-link-mine href="/contact"
+                                    :active="request()->is('contact')">Contact</x-nav-link-mine>
 
                             </div>
                         </div>
