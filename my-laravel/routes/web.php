b96a38d4de74697d5e9e97908bc04338051560cc
aristos <aristos.aresti@gmail.com>
2024-11-10 21:48:06 +0200

    Refactor job routes to use Route::controller for cleaner syntax and add views for home, contact, and about pages

--- my-laravel/routes/web.php
+++ my-laravel/routes/web.php
@@ -4,26 +4,33 @@ use App\Http\Controllers\JobController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
+// Route::get('/', function () {
+//     return view('home');
+// });
+
+Route::view('/', 'home');
+Route::view('/contact', 'contact');
+Route::view('/about', 'about');
+
+// Route::get('/contact', function () {
+//     return view('contact');
+// });
+// Route::get('/about', function () {
+//     return view('about');
+// });
+
+Route::controller(JobController::class)->group(function () {
+    Route::get('/jobs', 'index');
+    Route::get('/jobs/create', 'create');
+    Route::get('/jobs/{job}', 'show');
+    Route::post('/jobs', 'store');
+    Route::get('/jobs/{job}/edit', 'edit');
+    Route::patch('/jobs/{job}', 'update');
+    Route::delete('/jobs/{job}', 'destroy');
 });
 
-Route::get('/contact', function () {
-    return view('contact');
-});
-
-Route::get('/about', function () {
-    return view('about');
-});
-
-
-Route::get('/jobs', [JobController::class, 'index']);
-Route::get('/jobs/create', [JobController::class, 'create']);
-Route::get('/jobs/{job}', [JobController::class, 'show']);
-Route::post('/jobs', [JobController::class, 'store']);
-Route::get('/jobs/{job}/edit', [JobController::class, 'edit']);
-Route::patch('/jobs/{job}', [JobController::class, 'update']);
-Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
+// shorthand for the above
+// Route::resource('jobs', JobController::class);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
