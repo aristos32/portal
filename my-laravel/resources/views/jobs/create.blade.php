fa32535d3a567e10e38031eba685bd390c778c0a
aristos <aristos.aresti@gmail.com>
2024-11-13 08:23:32 +0200

    Refactor job creation form to use custom Blade components for improved readability and maintainability

--- my-laravel/resources/views/jobs/create.blade.php
+++ my-laravel/resources/views/jobs/create.blade.php
@@ -26,55 +26,32 @@
 
                 <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
 
-                    <div class="sm:col-span-4">
-                        <label for="title" class="block text-sm/6 font-medium text-gray-900">Title</label>
+                    <x-test.form-field>
+                        <x-test.form-label for="title"> Title </x-test.form-label>
+
                         <div class="mt-2">
-                            <div
-                                class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                <input type="text" name="title" id="title"
-                                    class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm/6"
-                                    placeholder="Software Engineer" required>
-                            </div>
-
-                            @error('title')
-                                <p class="text-xs text-red-600">{{ $message }}</p>
-                            @enderror
+                            <x-test.form-input type="text" name="title" id="title" placeholder="Software Engineer"
+                                required />
+                            <x-test.form-error name="title" />
                         </div>
-                    </div>
+                    </x-test.form-field>
 
-                    <div class="sm:col-span-4">
-                        <label for="salary" class="block text-sm/6 font-medium text-gray-900">Salary</label>
+                    <x-test.form-field>
+                        <x-test.form-label for="salary"> Salary </x-test.form-label>
+
                         <div class="mt-2">
-                            <div
-                                class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                <input type="text" name="salary" id="salary"
-                                    class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm/6"
-                                    placeholder="$50,00 per year" required>
-                            </div>
-
-                            @error('salary')
-                                <p class="text-xs text-red-600">{{ $message }}</p>
-                            @enderror
+                            <x-test.form-input type="text" name="salary" id="salary" placeholder="$50,000" required />
+                            <x-test.form-error name="title" />
                         </div>
-                    </div>
+                    </x-test.form-field>
+
                 </div>
-
-                <!-- <div class="mt-10">
-                    @if ($errors->any())
-                        <ul>
-                            @foreach ($errors->all() as $error)
-                                <li class="text-red-600">{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    @endif
-                </div> -->
 
             </div>
 
             <div class="mt-6 flex items-center justify-end gap-x-6">
                 <button type="button" class="text-sm/6 font-semibold text-gray-900">Cancel</button>
-                <button type="submit"
-                    class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+                <x-test.form-button>Save</x-test.button>
             </div>
     </form>
 
