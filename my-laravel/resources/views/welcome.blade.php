ef1698b4f3e46451438924e383cb9c9aecf215b6
Aristos Aresti <aristos.aresti@gmail.com>
2024-12-12 23:19:53 +0200

    some changes

---
+++ my-laravel/resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<x-layout>
+    <section>
+        <h3>Top Jobs</h3>
+        <div class="p-4 bg-white">
+            <div>Laracasts</div>
+            <div>
+                <h3>Video Producer</h3>
+                <p>Full Time - $150,000/year</p>
+            </div>
+
+            <div>
+                <div>
+                    <a href="#">Tag</a>
+                    <a href="#">Tag</a>
+                    <a href="#">Tag</a>
+                </div>
+
+                <img src="https://via.placeholder.com/150" alt="Company Logo">
+            </div>
+    </section>
+</x-layout>
--- update.sh
+++ update.sh
@@ -5,20 +5,20 @@ git pull
 echo "Installing Composer Dependencies"
 docker-compose run --rm composer install
 
-echo "Installing NPM Dependencies"
-docker-compose exec app npm install 
-
 echo 'stoping docker services'
 docker-compose down
 
 echo "starting docker services"
 docker-compose up -d
 
-echo "Running Migrations"
-docker-compose exec app php artisan migrate
-
 echo "Setting permissions inside the container"
 docker-compose exec app chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache && docker-compose exec app chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+
+echo "Installing NPM Dependencies"
+docker-compose exec app npm install 
+
+echo "Running Migrations"
+docker-compose exec app php artisan migrate
 
 # echo "setting file ownership"
 # sudo chown -R aristos:aristos *
