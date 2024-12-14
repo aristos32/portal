2d9562d7293e1d0565569ad0fbdfad818352940b
Aristos Aresti <aristos.aresti@gmail.com>
2024-12-14 22:59:38 +0200

    Enhance job card and tag components; update job tags with specific roles and improve tag styling for better responsiveness. Revamp welcome page layout; add job search form and section heading for latest jobs.

--- my-laravel/resources/views/components/tag.blade.php
+++ my-laravel/resources/views/components/tag.blade.php
@@ -1,1 +1,15 @@
-<a href="#" class="bg-white/10 hover:bg-red/25 px-2 py-1 font-bold rounded-b1 text-2xs">{{$slot}}</a>
+@props(['size' => 'base'])
+
+@php
+$classes ="bg-white/10 hover:bg-red/25 font-bold rounded-x1 transition-colors duration-300";
+
+if($size == 'base'){
+$classes .=" px-5 py-2 text-sm";
+}
+
+if($size == 'small'){
+$classes .=" px-2 py-1 text-2xs";
+
+}
+@endphp
+<a href="#" class="{{$classes}}">{{$slot}}</a>
