0c3a3665d1b6aa0448e7f897c5861ea941d9edbd
Aristos Aresti <aristos.aresti@gmail.com>
2025-03-19 12:14:07 +0200

    Fix layout formatting in home and welcome views; ensure proper indentation and add missing newlines. Update update.sh to run NPM Dev in the background.

--- my-laravel/resources/views/welcome.blade.php
+++ my-laravel/resources/views/welcome.blade.php
@@ -3,15 +3,15 @@
         Home Page
     </x-slot:heading>
     <div class="space-y-10">
-<section class="text-center  ">
-    <h1 class="font-bold text-4xl">Find your Next Job</h1>
+        <section class="text-center  ">
+            <h1 class="font-bold text-4xl">Find your Next Job</h1>
 
-    <form method="POST" action="/search" class="mt-6">
-        <input type="text" placeholder="Web Developer..."
-            class="rounded-xl bg-white/5 border-white/10 px-5 py-4 w-full max-w-l">
-    </form>
-</section>
-<section class="pt-10">
+            <form method="POST" action="/search" class="mt-6">
+                <input type="text" placeholder="Web Developer..."
+                    class="rounded-xl bg-white/5 border-white/10 px-5 py-4 w-full max-w-l">
+            </form>
+        </section>
+        <section class="pt-10">
             <x-section-heading>Latest Jobs</x-section-heading>
 
             <div class="grid lg:grid-cols-3 gap-4 mt-6">
