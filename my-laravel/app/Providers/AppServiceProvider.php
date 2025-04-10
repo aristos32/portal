f5d7e5353a106ed02ff56a37d9d7c41fc9aa6bbe
Aristos Aresti <aristos.aresti@gmail.com>
2025-04-10 16:54:43 +0300

    Add comment for clarity on locale determination in AppServiceProvider

--- my-laravel/app/Providers/AppServiceProvider.php
+++ my-laravel/app/Providers/AppServiceProvider.php
@@ -26,6 +26,7 @@ class AppServiceProvider extends ServiceProvider
 
         // This assumes the current locale is set in the URL (e.g., from the first URL segment)
         // or falls back to your config setting (usually 'en').
+        // https://chatgpt.com/c/67f0c907-43c0-8009-8bbc-41eaba096800
         $locale = request()->segment(1) ?: config('app.locale', 'en');
 
         URL::defaults(['locale' => $locale]);
