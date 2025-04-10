a155d68411b3beaf321bc953a129242123ca3f90
Aristos Aresti <aristos.aresti@gmail.com>
2025-04-10 16:47:30 +0300

    Remove unused helper function for locale routing from autoload configuration

--- my-laravel/app/Helpers/helper.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if (!function_exists('locale_route')) {
-    function locale_route(string $name, array $params = [], bool $absolute = true): string
-    {
-        return route($name, array_merge(['locale' => app()->getLocale()], $params), $absolute);
-    }
-}
