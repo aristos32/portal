17b5d04e4a2b25775689b7622690d0be12375e88
Aristos Aresti <aristos.aresti@gmail.com>
2025-04-18 09:11:30 +0300

    Enhance Customer model documentation with detailed class description

--- my-laravel/app/Models/Customer.php
+++ my-laravel/app/Models/Customer.php
@@ -5,6 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * This is the customer of the business.
+ * It cannot login to the system, as opposed to the user.
+ */
 class Customer extends Model
 {
     /** @use HasFactory<\Database\Factories\CustomerFactory> */
