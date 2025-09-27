b44005340d59e2bcd944595d66b67b12da4a6aa7
Aristos Aresti <aristos.aresti@gmail.com>
2025-09-27 10:57:01 +0300

    Refactor web routes to streamline controller usage
    - Grouped routes for ProfileController and SearchController under their respective controllers for improved organization and readability.
    - Updated route definitions to use string-based method references for consistency.

--- my-laravel/routes/web.php
+++ my-laravel/routes/web.php
@@ -55,12 +55,19 @@ Route::group([
 
 
     Route::middleware('auth')->group(function () {
-        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+        Route::controller(ProfileController::class)->group(function(){
 
-        Route::post('/search', [SearchController::class, 'search'])->name('search');
-        Route::get('/addresses', [AddressController::class, 'index'])->name('addresses.index');
+            Route::get('/profile/edit', 'edit')->name('profile.edit');
+            Route::patch('/profile', 'update')->name('profile.update');
+            Route::delete('/profile', 'destroy')->name('profile.destroy');
+            Route::get('/addresses', 'index')->name('addresses.index');
+
+        });
+
+
+        Route::controller(SearchController::class)->group(function(){
+            Route::post('/search', 'search')->name('search');
+        });
 
         // customer routes
         Route::controller(CustomerController::class)->group(function () {
