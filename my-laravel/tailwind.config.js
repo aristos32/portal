d490789b4949b4fa855759b3194f8f145cef7848
Aristos Aresti <aristos.aresti@gmail.com>
2026-01-06 21:58:43 +0200

    Update Tailwind CSS configuration to clarify font size definition for '2xs'

--- my-laravel/tailwind.config.js
+++ my-laravel/tailwind.config.js
@@ -15,7 +15,7 @@ export default {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
             fontSize: {
-                '2xs': '.625rem',
+                '2xs': '.625rem',// 10px
             },
 
         },
